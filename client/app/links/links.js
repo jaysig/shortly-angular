be99297e116c64f34254269e276a886f880c2edd
Jaysig <jasontsigmon@gmail.com>
2015-11-06 17:59:02 -0500

    working on links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,8 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.links
+  $scope.addLink = function(link){
+    $scope.links.push({url: url}); //need to work on
+  };
 });
