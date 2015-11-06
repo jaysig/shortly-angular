f52565bfc5c2839f9c703920dcf030fd7dd1edf3
Jaysig <jasontsigmon@gmail.com>
2015-11-06 18:03:55 -0500

    fixed shorten code loc

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,8 +2,7 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
-  $scope.links
-  $scope.addLink = function(link){
-    $scope.links.push({url: url}); //need to work on
-  };
+  $scope.links = [];
+  
+
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,8 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.addLink = function(link){
+    $scope.links.push({url: url}); //need to work on
+    $scope.newLink = '';
+  };
 });
